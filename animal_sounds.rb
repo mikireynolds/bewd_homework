c86dc0c92444c068062600679a475824653c9581
Miki <miki.reynolds@gmail.com>
2015-04-03 15:21:30 -0700

    final clean version of week 1 homework

--- animal_sounds.rb
+++ animal_sounds.rb
@@ -2,18 +2,15 @@ puts "Hello! Please name an animal."
 
 animal = gets.chomp
 
-puts "What sound does the " + animal.downcase.strip + " make?"
+puts "What sound does the " + animal.downcase.strip + " make?" 
+#What sound does the ____ make?
 
 sound = gets.chomp
 
 puts "How many times does the " + animal.downcase.strip + " go " + sound.downcase.strip + "?"
+#How many times does the ____ go ___?
 
 number = gets.chomp.to_i
 
-#final = ("#{sound.downcase.strip}#{","} " * number)
-
-#puts "The " + animal.downcase.strip + " goes " + final + "."
-
-#puts "The " + animal.downcase.strip + " goes " + ("#{sound.downcase.strip.}#{","} " * number)
-
 puts "The " + animal.downcase.strip + " goes " + (sound.downcase.strip + "," + " ") * (number-1) + sound.downcase.strip + " muthafuckaaa!!"
+#The ___ goes ____.
